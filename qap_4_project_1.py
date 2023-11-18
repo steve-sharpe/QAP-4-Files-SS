60d24dd213d94bbc583baaa6f26c2ea6340f7ba1
steve-sharpe <150041936+steve-sharpe@users.noreply.github.com>
2023-11-17 21:45:33 -0330

    Creating repository for QAP 4

---
+++ qap_4_project_1.py
@@ -0,0 +1,284 @@
+# This is a program to enter and calculate new insurance policy information for its customers.
+# Program Author: Steve Sharpe
+# Program Date: 2023-11-17
+
+# Import required libraries.
+
+import datetime
+import format_values as FF
+
+# Set up program contrants.
+
+BASIC_PREMIUM = 869.00
+ADD_CAR_DISC = 0.75
+EXTRA_LIABILTY = 130.00
+GLASS_COVERAGE = 86.00
+LOANER_CAR_COVERAGE = 58.00
+HST_RATE = 0.15
+MONTHLY_PROC_FEES = 39.99
+INV_DATE = datetime.datetime.now()
+
+# Set up program functions.
+
+
+def InsurPremium(NumCars):
+    # Function to calculate the insurance premium based on number of cars.
+    if NumCars >= 1:
+        InsurPremium = BASIC_PREMIUM + ((BASIC_PREMIUM * ADD_CAR_DISC) * (NumCars - 1))
+    return InsurPremium
+
+
+def ExtraCosts(LiabilityOption, GlassOption, LoanerOption):
+    # Function to calculate the extra costs based on the options selected.
+    ExtraCosts = 0
+    if LiabilityOption == "Y":
+        ExtraCosts += EXTRA_LIABILTY
+    if GlassOption == "Y":
+        ExtraCosts += GLASS_COVERAGE
+    if LoanerOption == "Y":
+        ExtraCosts += LOANER_CAR_COVERAGE
+    return ExtraCosts
+
+
+def CalcHST(HST_RATE, InsurPremium, ExtraCosts):
+    # Function to calculate the HST.
+    HST = (InsurPremium + ExtraCosts) * HST_RATE
+    return HST
+
+
+def TotalCost(InsurPremium, ExtraCosts, HST, DownPayment):
+    # Function to calculate the total cost including down payment.
+    TotalCost = (InsurPremium + ExtraCosts + HST) - DownPayment
+    return TotalCost
+
+
+def MonthlyCost(TotalCost):
+    # Function to calculate the monthly cost.
+    MonthlyCost = (TotalCost / 8) + MONTHLY_PROC_FEES
+    return MonthlyCost
+
+
+def CalcPayDate(InvDate):
+    # Calculate the payment date as the first of each month.
+
+    if InvDate.day > 1:
+        if InvDate.month == 12:
+            PayDate = datetime.datetime(InvDate.year + 1, 1, 1)
+        else:
+            PayDate = datetime.datetime(InvDate.year, InvDate.month + 1, 1)
+    else:
+        PayDate = InvDate
+
+    return PayDate
+
+
+# Start the main program.
+
+NextPolicyNum = "1944"
+CustFirstName = "Michael"
+CustLastName = "Myers"
+StrAdd = "123 Elm Street"
+City = "Haddonfield"
+Province = "ON"
+PostalCode = "A1A 1A1"
+Phone = "416-555-1234"
+NumCars = 2
+LiabilityOption = "Y"
+GlassOption = "Y"
+LoanerOption = "Y"
+PayOption = "D"
+DownPayment = 100.00
+PrevClaimAmtLst = [1000.00, 500.00, 250.00]
+PrevClaimDateLst = ["2021-01-01", "2021-02-01", "2021-03-01"]
+
+# while True:
+#     NextPolicyNum = "1944"
+#     while True:
+#         CustFirstName = input("Enter the customer's first name: ").title()
+#         if CustFirstName == "":
+#             print("You must enter a first name.")
+#         else:
+#             break
+
+#     while True:
+#         CustLastName = input("Enter the customer's last name: ").title()
+#         if CustLastName == "":
+#             print("You must enter a last name.")
+#         else:
+#             break
+
+#     while True:
+#         StrAdd = input("Enter the customer's street address: ").title()
+#         if StrAdd == "":
+#             print("You must enter a street address.")
+#         else:
+#             break
+
+#     while True:
+#         City = input("Enter the customer's city: ").title()
+#         if City == "":
+#             print("You must enter a city.")
+#         else:
+#             break
+
+#     while True:
+#         ProvList = [
+#             "NL",
+#             "NS",
+#             "NB",
+#             "PE",
+#             "QC",
+#             "ON",
+#             "MB",
+#             "SK",
+#             "AB",
+#             "BC",
+#             "YT",
+#             "NT",
+#             "NU",
+#         ]
+#         Province = input("Enter the customer's province (XX): ").upper()
+#         if Province == "":
+#             print("You must enter a province.")
+#         elif len(Province) != 2:
+#             print("You must enter a province in XX format.")
+#         elif Province not in ProvList:
+#             print("You must enter a valid province.")
+#         else:
+#             break
+
+#     while True:
+#         Phone = input("Enter the customer's phone number (999-999-9999): ")
+#         if Phone == "":
+#             print("You must enter a phone number.")
+#         elif len(Phone) != 12:
+#             print("You must enter a phone number in 999-999-9999 format.")
+#         elif Phone[3] != "-" or Phone[7] != "-":
+#             print("You must enter a phone number in 999-999-9999 format.")
+#         elif (
+#             Phone[0:3].isnumeric() == False
+#             or Phone[4:7].isnumeric() == False
+#             or Phone[8:12].isnumeric() == False
+#         ):
+#             print("You must enter a phone number in 999-999-9999 format.")
+#         else:
+#             break
+
+#     while True:
+#         NumCars = input("Enter the number of cars: ")
+#         if NumCars == "":
+#             print("You must enter a number of cars.")
+#         elif NumCars.isalpha() == True:
+#             print("You must enter a number.")
+#         elif int(NumCars) <= 0:
+#             print("Number cannot be less than 1.")
+#         else:
+#             break
+
+#     while True:
+#         LiabilityOption = input(
+#             f"Would you like optional extra liability up to $1,000,000 at a cost of {FF.FDollar2( EXTRA_LIABILTY)} (Y or N)?: "
+#         ).upper()
+#         if LiabilityOption == "":
+#             print("You must enter Y for YES or N for NO.")
+#         elif LiabilityOption != "Y" and LiabilityOption != "N":
+#             print("You must enter Y for YES or N for No.")
+#         else:
+#             break
+
+#     while True:
+#         GlassOption = input(
+#             f"Would you like optional glass coverage at a cost of {FF.FDollar2(GLASS_COVERAGE)} (Y or N)?: "
+#         ).upper()
+#         if GlassOption == "":
+#             print("You must enter Y for YES or N for NO.")
+#         elif GlassOption != "Y" and GlassOption != "N":
+#             print("You must enter Y for YES or N for No.")
+#         else:
+#             break
+
+#     while True:
+#         LoanerOption = input(
+#             f"Would you like optional loaner car coverage at a cost of {FF.FDollar2(LOANER_CAR_COVERAGE)} (Y or N)?: "
+#         ).upper()
+#         if LoanerOption == "":
+#             print("You must enter Y for YES or N for NO.")
+#         elif LoanerOption != "Y" and LoanerOption != "N":
+#             print("You must enter Y for YES or N for No.")
+#         else:
+#             break
+
+#     while True:
+#         PayList = ["F", "M", "D"]
+#         PayOption = input(
+#             "Would you like to pay in (F)ull, (M)onthly, or monthly with a (D)own payment (F, M, or D)?: "
+#         ).upper()
+#         if PayOption == "":
+#             print("You must enter F for FULL, M for MONTHLY, or D for DOWN.")
+#         elif PayOption not in PayList:
+#             print("You must enter F for FULL, M for MONTHLY, or D for DOWN.")
+#         elif PayOption == "D":
+#             DownPayment = float(input("Enter the down payment amount: "))
+#             if DownPayment < 0:
+#                 print("Down payment cannot be less than 0.")
+#             else:
+#                 break
+#         else:
+#             break
+
+#     while True:
+#         PrevClaimOption = input(
+#             "Is there a previous claim to enter? (Y or N)?: "
+#         ).upper()
+#         while True:
+#             PrevClaimAmtLst = []
+#             PrevClaimDateLst = []
+#             if PrevClaimOption == "":
+#                 print("You must enter Y for YES or N for NO.")
+#             elif PrevClaimOption != "Y" and PrevClaimOption != "N":
+#                 print("You must enter Y for YES or N for No.")
+#             elif PrevClaimOption == "Y":
+#                 PrevClaimAmt = float(input("Enter the previous claim amount: "))
+#                 PrevClaimDate = input("Enter the previous claim date (YYYY-MM-DD): ")
+#                 PrevClaimAmtLst.append(PrevClaimAmt)
+#                 PrevClaimDateLst.append(PrevClaimDate)
+#                 PrevClaimOption = input(
+#                     "Is there another previous claim to enter? (Y or N)?: "
+#                 ).upper()
+#                 if PrevClaimOption == "":
+#                     print("You must enter Y for YES or N for NO.")
+#                 elif PrevClaimOption != "Y" and PrevClaimOption != "N":
+#                     print("You must enter Y for YES or N for No.")
+#                 if PrevClaimOption == "Y":
+#                     continue
+#                 elif PrevClaimOption == "N":
+#                     break
+#             elif PrevClaimOption == "N":
+#                 break
+#             break
+#         break
+#     break
+
+print("***********************************************************************")
+print(
+    f"*****  Policy Number: {NextPolicyNum}  **********  Invoice Date: {FF.FDateS(INV_DATE)}  *****"
+)
+print("***********************************************************************")
+print(
+    f"**  Customer:  {CustFirstName} {CustLastName}             # of Cars: {NumCars} "
+)
+print(
+    f"**  Address:   {StrAdd}{'Liability:'.rjust(50 - len(StrAdd))} {LiabilityOption}"
+)
+print(f"**             {City}, {Province:<15}Glass:     {GlassOption} ")
+print(f"**             {PostalCode:<20}Loaner:    {LoanerOption} ")
+print()
+print(f"**  Phone:   {Phone}")
+print(f"Number of Cars: {NumCars}")
+print(f"Optional Extra Liability: {LiabilityOption}")
+print(f"Optional Glass Coverage: {GlassOption}")
+print(f"Optional Loaner Car Coverage: {LoanerOption}")
+print(f"Payment Option: {PayOption}")
+print(f"Down Payment: {FF.FDollar2(DownPayment)}")
+print(f"Previous Claims: {PrevClaimAmtLst}")
+print(f"Previous Claim Dates: {PrevClaimDateLst}")
